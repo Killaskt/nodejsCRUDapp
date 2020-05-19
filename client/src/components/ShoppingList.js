4f63fb7258fe9d45c7d6a6da2cfd3036743e9c49
killaskt <thegimreper@gmail.com>
2020-05-19 10:44:36 -0400

    Shopping List & etc!
    Added Shopping List display, displays the items in a list format, not connected to backend (utilizes fake items for now). Got CSS Reactstrap Transitions hooked up for insert and delete working... oh yeah Delete is semi functional (still need to hit the rest api for an official db delete.

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import {v4 as uuid} from 'uuid';
+
+class ShoppingList extends Component {
+    state = {
+        items: [
+            {
+                id: uuid(),
+                name: "Soy"
+            }, 
+            {
+                id: uuid(),
+                name: 'Milk'
+            }, 
+            {
+                id: uuid(),
+                name: "Celery"
+            }, 
+            {
+                id: uuid(),
+                name: "Potatas"
+            }, 
+        ]
+    }
+
+    render() {
+        const {items} = this.state;
+        return (
+            <Container>
+                <Button 
+                    color="dark"
+                    style={{marginBottom: '2rem'}}
+                    onClick={() => {
+                        const name = prompt("Enter Item:")
+                        if (name) {
+                            this.setState(state => ({
+                                items: [...state.items, {id: uuid(), name: name}] //name: name can simply be just 'name' in ES6
+                            }))
+                        }
+                    }}
+                >Add Item</Button>
+
+                <ListGroup>
+                    <TransitionGroup className="shopping-list">
+                        {items.map(({id, name}) => ( 
+                            <CSSTransition key={id} timeout={500} classNames="fade">
+                                <ListGroupItem>
+                                    <Button 
+                                    className="remove-Btn"
+                                    color ="danger"
+                                    size = "sm"
+                                    onClick={() => {
+                                        this.setState(state => ({
+                                            items: state.items.filter(item => item.id !== id)
+                                        }));
+                                    }}>
+                                        &times;</Button>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
+            </Container>
+        )
+    }
+}
+
+export default ShoppingList;
